806390add5fedf56f988f1bf88f31f8f7382b739
Emmanuel Akintaro <>
2019-05-23 23:41:22 +0100

    Fix to content page

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -1,12 +1,10 @@
-<?php while(have_posts()):the_post();?>
-	<div class="flex-row flex-wrap wide padded">
-		<div class="flex flex-1">
-			<h2><?php the_title();?></h2>
-		</div>
-		<div class="flex flex-2-3">
-			<article class="body-content">
-				<?php the_content();?>	
-			</article>
-		</div>
+<div class="flex-row flex-wrap wide padded">
+	<div class="flex flex-1">
+		<h2><?php the_title();?></h2>
 	</div>
-<?php endwhile;?>
+	<div class="flex flex-2-3">
+		<article class="body-content">
+			<?php the_content();?>	
+		</article>
+	</div>
+</div>
